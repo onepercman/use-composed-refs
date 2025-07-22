14f10b22c436a748eea99784ad7ce421fc4d021c
onepercman <onepercman@gmail.com>
2025-07-22 11:35:53 +0700

    🚀 update version

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.tsx'],
-  format: ['cjs', 'esm'],
-  external: ['tailwindcss'],
-  dts: true,
-  sourcemap: true,
+  format: ['esm', 'cjs'], // Xuất ra cả ESM và CJS
+  dts: true, // Xuất file .d.ts cho TypeScript
+  sourcemap: true, // Xuất sourcemap
+  clean: true, // Xóa thư mục dist trước khi build
 })
